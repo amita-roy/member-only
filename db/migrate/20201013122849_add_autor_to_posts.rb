51639a227cd141f3d4ff437116e2fd9e3f0df446
Amita <sardana.amita2@gmail.com>
2020-10-13 18:08:33 +0530

    Show author name when signed_in

---
+++ db/migrate/20201013122849_add_autor_to_posts.rb
@@ -0,0 +1,5 @@
+class AddAutorToPosts < ActiveRecord::Migration[6.0]
+  def change
+    add_column :posts, :author, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_120006) do
+ActiveRecord::Schema.define(version: 2020_10_13_122849) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_10_13_120006) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
   create_table "users", force: :cascade do |t|
