67e3dff249f2ef5ab5987ab6a2396dfe06d57c47
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-10-13 13:24:41 +0300

    Add before_action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_094635) do
+ActiveRecord::Schema.define(version: 2020_10_13_100844) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
